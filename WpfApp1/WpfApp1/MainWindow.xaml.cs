0df6a2fc5941f108ce6f08b984c07ab842bb4059
Marvin <mj03505@georgiasouthern.edu>
2019-07-19 20:37:37 -0400

    Added more UI Controls and Implemented Functions
    UI Controls were added and the functions for the radio buttons were implemented. I just need to set up a small window with a table of inactive companies to appear when ran and also possibly an add feature to add the desired company to track or add another feed for an existing company.
    
    I'll get that done tomorrow and also look into unit testing methods.

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ServiceModel.Syndication;
+using System.Xml;
 
 namespace WpfApp1
 {
@@ -21,6 +23,20 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Dictionary for Companies and their URLs
+        IDictionary<String, dynamic> CompanyFeeds = new Dictionary<String, dynamic>();
+
+        //List Collection for Company URLs
+        List<String> BBCUrl = new List<String>();
+        List<String> RealTimeUrl = new List<String>();
+        List<String> BillSimmUrl = new List<String>();
+        List<String> DianeUrl = new List<String>();
+
+        //List Collection for the slacking companies
+        List<String> SlackingCompanies = new List<String>();
+
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,25 +47,101 @@ namespace WpfApp1
             
         }
 
-        public List<String> InactiveCompanies(IDictionary dic)
+        //Required Function
+        public List<String> InactiveCompanies(SelectedDatesCollection dates)
         {
-            List<String> com = new List<String>();
-            return com;
+            //Iterate through dictionary
+            foreach (KeyValuePair<string, dynamic> entry in CompanyFeeds)
+            {
+                //Load Feed
+                var r = XmlReader.Create(entry.Value);
+                var feed = SyndicationFeed.Load(r);
+
+                //Iterate through retrieved posts
+                foreach (SyndicationItem post in feed.Items)
+                {
+                    //If there is a post published in the date range continue
+                    //If the company hasnt posted in one feed but has in another we will remove it from the slacking pile
+                    if (dates.Contains(post.PublishDate.UtcDateTime))
+                    {
+                        if (SlackingCompanies.Contains(entry.Key))
+                        {
+                            SlackingCompanies.Remove(entry.Key);
+                        }
+
+                        continue;
+                    }
+                    else
+                    {
+                        //Add the company name to the slacking group if they havent posted 
+                        SlackingCompanies.Add(entry.Key);
+                    }
+
+                }
+
+            }
+            
+
+            return SlackingCompanies;
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        //BBC Radio Button Function
+        private void BBC_RadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            BBCUrl.Add("https://www.bbc.com/news/10628494");
+            if (BBCTrack.IsChecked == true)
+            {
+                CompanyFeeds.Add("BBC", BBCUrl);
+            }
+                
+        }
+    
+        //Real Time Radio Button Function
+        private void RealTime_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RealTimeUrl.Add("http://billmaher.hbo.libsynpro.com/rss");
+            if (RealTimeTrack.IsChecked == true)
+            {
+                foreach (String i in RealTimeUrl){
+                    CompanyFeeds.Add("Real Time", RealTimeUrl.IndexOf(i));
+                }
+            }
 
         }
 
-        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        //Bill Simmons Radio Button Function
+        private void BillSim_RadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            BillSimmUrl.Add("https://rss.art19.com/the-bill-simmons-podcast");
+            if (BillSimTrack.IsChecked == true)
+            {
+                foreach (String i in BillSimmUrl)
+                {
+                    CompanyFeeds.Add("Bill Simmons", BillSimmUrl.IndexOf(i));
+                }
+            }
 
         }
 
-        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        //Diane Rehm Radio Button Function
+        private void Diane_RadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            DianeUrl.Add("https://dianerehm.org/rss/npr/dr_podcast.xml");
+            if (DianeTrack.IsChecked == true)
+            {
+                foreach (String i in DianeUrl)
+                {
+                    CompanyFeeds.Add("Diane Rehm", DianeUrl.IndexOf(i));
+                }
+            }
 
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedDatesCollection dates = DateRange.SelectedDates;
+            InactiveCompanies(dates);
         }
     }
 }
